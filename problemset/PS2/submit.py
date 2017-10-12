4f0abc92f4e33f97850bb11a21e3d41a6e142f80
taragb <taragb@gmail.com>
2017-10-12 15:01:05 -0700

    added ps2 submission instructions

---
+++ problemset/PS2/submit.py
@@ -0,0 +1,38 @@
+# copy and paste your SQL queries into each of the below variables (omit %sql)
+# note: do NOT rename variables
+
+Q1a = '''
+
+'''
+
+Q1b = '''
+
+'''
+
+Q1c = '''
+
+'''
+
+Q2a_F1 = []
+
+Q2a_F2 = []
+
+Q2b_ = '''
+
+'''
+
+Q3a = '''
+
+'''
+
+Q3b = '''
+
+'''
+
+Q3c = '''
+
+'''
+
+#################################
+# do NOT modify below this line #
+#################################
